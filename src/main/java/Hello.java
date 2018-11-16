68191532932e79ee926e84d1d4f74979e26cc0ac
wijayaerick <wijaya.erick52@gmail.com>
2018-11-16 12:50:24 +0700

    hello

---
+++ src/main/java/Hello.java
@@ -0,0 +1,21 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.InputStream;
+
+public class Hello {
+
+    public static void main(String args[]) throws Exception {
+        InputStream is = Hello.class.getResourceAsStream("hello.txt");
+        CharStream helloCharStream = CharStreams.fromStream(is);
+        HelloLexer lexer = new HelloLexer(helloCharStream);
+        CommonTokenStream tokens = new CommonTokenStream( lexer );
+        HelloParser parser = new HelloParser( tokens );
+        ParseTree tree = parser.r();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk( new HelloWalker(), tree );
+    }
+}
