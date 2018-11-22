bed04577575f2af4612b76fb4e9613f0389aa47b
roselinapradjanata <13515034@std.stei.itb.ac.id>
2018-11-22 13:30:33 +0700

    Add method toString to models

--- src/main/java/classscheduler/models/Classroom.java
+++ src/main/java/classscheduler/models/Classroom.java
@@ -3,12 +3,14 @@ package classscheduler.models;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Classroom {
 
     private String id;
--- src/main/java/classscheduler/models/Clazz.java
+++ src/main/java/classscheduler/models/Clazz.java
@@ -3,10 +3,12 @@ package classscheduler.models;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Clazz {
 
     private String name;
