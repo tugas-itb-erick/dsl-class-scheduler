d6531f315a828235a03bc90415d9306900583f04
Kezia Suhendra <keziasuhendra44@gmail.com>
2018-11-22 21:44:22 +0700

    add walker for classroom repository

--- src/main/java/ClassSchedulingWalker.java
+++ src/main/java/ClassSchedulingWalker.java
@@ -1,7 +1,8 @@
-import classscheduler.models.DayTime;
-import classscheduler.models.Lecturer;
+import classscheduler.models.Classroom;
 import classscheduler.repositories.*;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClassSchedulingWalker extends ClassSchedulingParserBaseListener {
@@ -34,6 +35,27 @@ public class ClassSchedulingWalker extends ClassSchedulingParserBaseListener {
     }
 
     public void createClassroom(List<ClassSchedulingParser.CreateClassroomContext> ctx) {
+        for (ClassSchedulingParser.CreateClassroomContext createClassroomContext : ctx) {
+            String id = "";
+            int capacity = 0;
+            List<String> facilities = new ArrayList<>();
+            for (ClassSchedulingParser.LineContext lineContext : createClassroomContext.createParam().line()) {
+                switch (lineContext.map().key().WORD().toString()) {
+                    case "id":
+                        id = lineContext.map().value().WORD(0).toString();
+                        break;
+                    case "capacity":
+                        capacity = Integer.parseInt(lineContext.map().value().WORD(0).toString());
+                        break;
+                    case "facilities":
+                        for (TerminalNode word : lineContext.map().value().WORD()) {
+                            facilities.add(word.toString());
+                        }
+                }
+            }
+            Classroom classroom = new Classroom(id, capacity, facilities);
+            classroomRepository.addClassroom(classroom);
+        }
     }
 
     public void createClass(List<ClassSchedulingParser.CreateClassContext> ctx) {
