b12361f4ea4e435eccb823bba8a2d6493a76da09
roselinapradjanata <13515034@std.stei.itb.ac.id>
2018-11-22 13:13:50 +0700

    Add class repositories

--- src/main/java/classscheduler/repositories/CourseRepository.java
+++ src/main/java/classscheduler/repositories/CourseRepository.java
@@ -3,16 +3,33 @@ package classscheduler.repositories;
 import classscheduler.models.Course;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 @Getter
 @Setter
+@ToString
 public class CourseRepository {
 
     private List<Course> courses;
 
-    public CourseRepository(List<Course> courses) {
-        this.courses = courses;
+    public CourseRepository() {
+        this.courses = new ArrayList<>();
+    }
+
+    public CourseRepository(Course[] courses) {
+        this.courses = new ArrayList<>(Arrays.asList(courses));
+    }
+
+    public CourseRepository(Collection<Course> courses) {
+        this.courses = new ArrayList<>(courses);
+    }
+
+    public void addCourse(Course course) {
+        this.courses.add(course);
     }
 }
