a0e072c4f73f98776979a282f58747aa15a7bcd7
Rachel Sidney <13515124@std.stei.itb.ac.id>
2018-11-22 17:50:19 +0700

    add repositories in class scheduling walker

--- src/main/java/ClassSchedulingWalker.java
+++ src/main/java/ClassSchedulingWalker.java
@@ -1,6 +1,26 @@
+import classscheduler.repositories.*;
+
 public class ClassSchedulingWalker extends ClassSchedulingParserBaseListener {
+    private ClassroomRepository classroomRepository;
+    private ClazzRepository clazzRepository;
+    private CourseRepository courseRepository;
+    private LecturerRepository lecturerRepository;
+    private TimeSlotRepository timeSlotRepository;
+
+    public ClassSchedulingWalker(ClassroomRepository classroomRepository,
+                                 ClazzRepository clazzRepository,
+                                 CourseRepository courseRepository,
+                                 LecturerRepository lecturerRepository,
+                                 TimeSlotRepository timeSlotRepository) {
+        this.classroomRepository = classroomRepository;
+        this.clazzRepository = clazzRepository;
+        this.courseRepository = courseRepository;
+        this.lecturerRepository = lecturerRepository;
+        this.timeSlotRepository = timeSlotRepository;
+    }
 
     public void enterCreateFacility(ClassSchedulingParser.CreateFacilityContext ctx) {
-        System.out.println(ctx.toString());
+        System.out.println(ctx.CREATE());
+        System.out.println(ctx);
     }
 }
